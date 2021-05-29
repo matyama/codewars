3170dc380046feaca6cb81706e8f14e1a406141f
Martin Matyášek <martin.matyasek@gmail.com>
2021-05-29 12:49:11 +0200

    Add Haskell version of Narcissistic number Kata

--- haskell/README.md
+++ haskell/README.md
@@ -3,6 +3,7 @@
 ## 6 kyu
 * [Consecutive strings](https://www.codewars.com/kata/56a5d994ac971f1ac500003e)
 * [Convert string to camel case](https://www.codewars.com/kata/517abf86da9663f1d2000003)
+* [Does my number look big in this?](https://www.codewars.com/kata/5287e858c6b5a9678200083c)
 * [Highest Scoring Word](https://www.codewars.com/kata/57eb8fcdf670e99d9b000272)
 
 ## 7 kyu
---
+++ haskell/src/Narcissistic.hs
@@ -0,0 +1,15 @@
+module Narcissistic where
+
+narcissistic :: Integral n => n -> Bool
+narcissistic n = value n == n
+
+-- Value of the narcissistic function with base 10
+value :: Integral n => n -> n
+value n = sum $ map (^k) ds
+  where ds = digits n
+        k = length ds
+
+-- Note: In this context it is fine to return no digits for 0
+digits :: Integral n => n -> [n]
+digits 0 = []
+digits n = digits (n `div` 10) ++ [n `mod` 10]
