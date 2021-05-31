c7fafe13fa5965f4fdcc8b5406427075218a8c97
Martin Matyášek <martin.matyasek@gmail.com>
2021-05-31 13:01:14 +0200

    Add memoization to Duplicate Encoder

--- haskell/src/Dups.hs
+++ haskell/src/Dups.hs
@@ -2,12 +2,9 @@ module Dups where
 
 import           Data.Char                      ( toLower )
 
--- TODO: memoization
--- https://kseo.github.io/posts/2017-01-14-memoization-in-hasekll.html
--- https://wiki.haskell.org/Memoization
--- https://wiki.haskell.org/Let_vs._Where
 duplicateEncode :: String -> String
-duplicateEncode str = [ if count c == 1 then '(' else ')' | c <- text ]
+duplicateEncode text = [ if occurences c == 1 then '(' else ')' | c <- codes ]
  where
-  text = map toLower str
-  count c = length $ filter (== c) text
+  codes      = map (fromEnum . toLower) text
+  occurences = (map count [0 ..] !!)
+    where count c = length $ filter (== c) codes
