e38e2edec712af55ff8c3a635782f8669a0f5902
Martin Matyášek <martin.matyasek@gmail.com>
2021-07-27 15:10:50 +0200

    Clean up class design in symbolic differentiation

--- python/codewars/differentiation.py
+++ python/codewars/differentiation.py
@@ -1,6 +1,7 @@
 import re
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Any, Dict, Optional, Type, Union, cast
 
 ARGS_EXPR = re.compile(r"^(?P<expr1>\(.*\)) (?P<expr2>\(.*\))$")
 
@@ -17,7 +18,7 @@ class Expr(ABC):
 
 @dataclass(frozen=True)
 class Const(Expr):
-    value: int
+    value: Union[int, float]
 
     def diff(self) -> Expr:
         return Const(0)
@@ -41,179 +42,274 @@ class Fn(Expr, ABC):
     def chain(self, inner: Expr) -> Expr:
         """Chains derivative of inner to self if inner's a funciton"""
         return (
-            Binary.new("*", arg1=inner.diff(), arg2=self)
+            Mul.apply(arg1=inner.diff(), arg2=self)
             if isinstance(inner, Fn)
             else self
         )
 
 
-@dataclass(frozen=True)
-class Unary(Fn):
-    fn: str
+# pylint: disable=too-few-public-methods
+class UnaryMeta:
+    _funcs: Dict[str, Type["Unary"]] = {}
+
+    @classmethod
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        # https://github.com/python/mypy/issues/4660
+        super().__init_subclass__(**kwargs)  # type: ignore
+        # This method is called on subclasses and since `cls` is a subclass of
+        # `UnaryMeta` which is only used as a base for `Unary`, this is safe.
+        cls._funcs[cls.__name__.lower()] = cast("Type[Unary]", cls)
+
+
+@dataclass(frozen=True)  # type: ignore
+class Unary(Fn, ABC, UnaryMeta):
     arg: Expr
 
-    def diff(self) -> Expr:
+    @classmethod
+    def new(cls, fn: str, arg: Expr) -> "Unary":
+        if fn not in cls._funcs:
+            raise ValueError("Unsupported function {fn}".format(fn=fn))
 
+        return cls._funcs[fn](arg)
+
+    @abstractmethod
+    def _diff(self) -> Expr:
+        raise NotImplementedError
+
+    def diff(self) -> Expr:
         if isinstance(self.arg, Const):
             return Const(0)
 
-        if self.fn == "cos":
-            df = Binary.new("*", arg1=Const(-1), arg2=Unary("sin", self.arg))
-
-        elif self.fn == "sin":
-            df = Unary("cos", self.arg)
-
-        elif self.fn == "tan":
-            df = Binary(
-                op="/",
-                arg1=Const(1),
-                arg2=Binary("^", arg1=Unary("cos", self.arg), arg2=Const(2)),
-            )
-
-        elif self.fn == "exp":
-            df = self
-
-        elif self.fn == "ln":
-            df = Binary.new("/", arg1=Const(1), arg2=self.arg)
-
-        else:
-            raise ValueError("Unsupported function {fn}".format(fn=self.fn))
-
-        return df.chain(inner=self.arg)
+        return self._diff().chain(inner=self.arg)
 
     def __str__(self) -> str:
-        return "({fn} {arg})".format(fn=self.fn, arg=self.arg)
+        return "({fn} {arg})".format(
+            fn=self.__class__.__name__.lower(), arg=self.arg
+        )
 
 
 @dataclass(frozen=True)
-class Binary(Fn):
-    op: str
+class Sin(Unary):
+    def _diff(self) -> Expr:
+        return Cos(self.arg)
+
+
+@dataclass(frozen=True)
+class Cos(Unary):
+    def _diff(self) -> Expr:
+        return Mul.apply(arg1=Const(-1), arg2=Sin(self.arg))
+
+
+@dataclass(frozen=True)
+class Tan(Unary):
+    def _diff(self) -> Expr:
+        return Div(
+            arg1=Const(1),
+            arg2=Pow(arg1=Cos(self.arg), arg2=Const(2)),
+        )
+
+
+@dataclass(frozen=True)
+class Exp(Unary):
+    def _diff(self) -> Expr:
+        return self
+
+
+@dataclass(frozen=True)
+class Ln(Unary):
+    def _diff(self) -> Expr:
+        return Div.apply(arg1=Const(1), arg2=self.arg)
+
+
+# pylint: disable=too-few-public-methods
+class BinaryMeta:
+    _ops: Dict[str, Type["Binary"]] = {}
+    _op: str
+
+    @classmethod
+    def __init_subclass__(
+        cls, /, op: Optional[str] = None, **kwargs: Any
+    ) -> None:
+        # https://github.com/python/mypy/issues/4660
+        super().__init_subclass__(**kwargs)  # type: ignore
+        # This method is called on subclasses and since `cls` is a subclass of
+        # `BinaryMeta` which is only used as a base for `Binary`, this is safe.
+        if op is not None:
+            cls._ops[op] = cast("Type[Binary]", cls)
+            cls._op = op
+
+
+@dataclass(frozen=True)  # type: ignore
+class Binary(Fn, BinaryMeta):
     arg1: Expr
     arg2: Expr
 
-    # pylint: disable=too-many-return-statements,too-many-branches
     @classmethod
-    def new(cls, op: str, arg1: Expr, arg2: Expr) -> Expr:
+    @abstractmethod
+    def _eval(cls, x: Const, y: Const) -> Const:
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def _apply(cls, arg1: Expr, arg2: Expr) -> Expr:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _diff(self) -> Expr:
+        raise NotImplementedError
+
+    @classmethod
+    def new(cls, op: str, arg1: Expr, arg2: Expr) -> "Binary":
+        if op not in cls._ops:
+            raise ValueError("Unsupported operation {op}".format(op=op))
+
+        return cls._ops[op](arg1, arg2)
+
+    @classmethod
+    def apply(cls, arg1: Expr, arg2: Expr) -> Expr:
         if isinstance(arg1, Const) and isinstance(arg2, Const):
-            # pylint: disable=eval-used
-            return Const(
-                value=eval(
-                    "{x} {op} {y}".format(
-                        x=arg1.value,
-                        op="**" if op == "^" else op,
-                        y=arg2.value,
-                    )
-                )
-            )
+            return cls._eval(arg1, arg2)
 
-        if op in "+-":
-            if isinstance(arg1, Const) and arg1.value == 0:
+        return cls._apply(arg1, arg2)
+
+    def diff(self) -> Expr:
+        if isinstance(self.arg1, Const) and isinstance(self.arg2, Const):
+            return Const(0)
+
+        return self._diff()
+
+    def __str__(self) -> str:
+        return "({op} {x} {y})".format(op=self._op, x=self.arg1, y=self.arg2)
+
+
+@dataclass(frozen=True)
+class Add(Binary, op="+"):
+    @classmethod
+    def _eval(cls, x: Const, y: Const) -> Const:
+        return Const(x.value + y.value)
+
+    @classmethod
+    def _apply(cls, arg1: Expr, arg2: Expr) -> Expr:
+        if isinstance(arg1, Const) and arg1.value == 0:
+            return arg2
+        if isinstance(arg2, Const) and arg2.value == 0:
+            return arg1
+        return cls(arg1, arg2)
+
+    def _diff(self) -> Expr:
+        return self.apply(self.arg1.diff(), self.arg2.diff())
+
+
+@dataclass(frozen=True)
+class Sub(Add, op="-"):
+    @classmethod
+    def _eval(cls, x: Const, y: Const) -> Const:
+        return Const(x.value - y.value)
+
+
+@dataclass(frozen=True)
+class Mul(Binary, op="*"):
+    @classmethod
+    def _eval(cls, x: Const, y: Const) -> Const:
+        return Const(x.value * y.value)
+
+    # pylint: disable=too-many-return-statements
+    @classmethod
+    def _apply(cls, arg1: Expr, arg2: Expr) -> Expr:
+        if isinstance(arg1, Const):
+            if arg1.value == 1:
                 return arg2
-            if isinstance(arg2, Const) and arg2.value == 0:
-                return arg1
-
-        if op == "*":
-            if isinstance(arg1, Const):
-                if arg1.value == 1:
-                    return arg2
-                if arg1.value == 0:
-                    return Const(0)
-                if (
-                    isinstance(arg2, Binary)
-                    and arg2.op == "/"
-                    and isinstance(arg2.arg1, Const)
-                ):
-                    return Binary(
-                        op=arg2.op,
-                        arg1=Const(arg1.value * arg2.arg1.value),
-                        arg2=arg2.arg2,
-                    )
-            if isinstance(arg2, Const):
-                if arg2.value == 1:
-                    return arg1
-                if arg2.value == 0:
-                    return Const(0)
-                if (
-                    isinstance(arg1, Binary)
-                    and arg1.op == "/"
-                    and isinstance(arg1.arg1, Const)
-                ):
-                    return Binary(
-                        op=arg1.op,
-                        arg1=Const(arg1.arg1.value * arg2.value),
-                        arg2=arg1.arg2,
-                    )
-
-        if op == "/":
-            if isinstance(arg1, Const) and arg1.value == 0:
+            if arg1.value == 0:
                 return Const(0)
-            if isinstance(arg2, Const) and arg2.value == 0:
-                raise ZeroDivisionError(
-                    "({op} {arg1} {arg2})".format(op=op, arg1=arg1, arg2=arg2)
+            if isinstance(arg2, Div) and isinstance(arg2.arg1, Const):
+                return Div(
+                    arg1=Const(arg1.value * arg2.arg1.value),
+                    arg2=arg2.arg2,
                 )
 
-        if op == "^" and isinstance(arg2, Const):
+        if isinstance(arg2, Const):
+            if arg2.value == 1:
+                return arg1
+            if arg2.value == 0:
+                return Const(0)
+            if isinstance(arg1, Div) and isinstance(arg1.arg1, Const):
+                return Div(
+                    arg1=Const(arg1.arg1.value * arg2.value),
+                    arg2=arg1.arg2,
+                )
+
+        return cls(arg1, arg2)
+
+    def _diff(self) -> Expr:
+        return Add.apply(
+            arg1=Mul.apply(self.arg1.diff(), self.arg2),
+            arg2=Mul.apply(self.arg1, self.arg2.diff()),
+        )
+
+
+@dataclass(frozen=True)
+class Div(Binary, op="/"):
+    @classmethod
+    def _eval(cls, x: Const, y: Const) -> Const:
+        return Const(x.value / y.value)
+
+    @classmethod
+    def _apply(cls, arg1: Expr, arg2: Expr) -> Expr:
+        if isinstance(arg1, Const) and arg1.value == 0:
+            return Const(0)
+        if isinstance(arg2, Const) and arg2.value == 0:
+            raise ZeroDivisionError(
+                "(/ {arg1} {arg2})".format(arg1=arg1, arg2=arg2)
+            )
+        return cls(arg1, arg2)
+
+    def _diff(self) -> Expr:
+        return Div.apply(
+            arg1=Sub.apply(
+                arg1=Mul.apply(self.arg1.diff(), self.arg2),
+                arg2=Mul.apply(self.arg1, self.arg2.diff()),
+            ),
+            arg2=Pow.apply(self.arg2, Const(2)),
+        )
+
+
+@dataclass(frozen=True)
+class Pow(Binary, op="^"):
+    @classmethod
+    def _eval(cls, x: Const, y: Const) -> Const:
+        return Const(x.value ** y.value)
+
+    @classmethod
+    def _apply(cls, arg1: Expr, arg2: Expr) -> Expr:
+        if isinstance(arg2, Const):
             if arg2.value == 0:
                 return Const(1)
             if arg2.value == 1:
                 return arg1
+        return cls(arg1, arg2)
 
-        return cls(op, arg1, arg2)
+    def _diff(self) -> Expr:
 
-    def diff(self) -> Expr:
-
-        if isinstance(self.arg1, Const) and isinstance(self.arg2, Const):
-            return Const(0)
-
-        if self.op in "+-":
-            return Binary.new(self.op, self.arg1.diff(), self.arg2.diff())
-
-        if self.op == "*":
-            return Binary.new(
-                op="+",
-                arg1=Binary.new("*", self.arg1.diff(), self.arg2),
-                arg2=Binary.new("*", self.arg1, self.arg2.diff()),
+        # Case (exponential): a^f(x)
+        if isinstance(self.arg1, Const):
+            df = Mul.apply(
+                arg1=self,
+                arg2=Ln(self.arg1),
             )
+            return df.chain(inner=self.arg2)
 
-        if self.op == "/":
-            return Binary.new(
-                op="/",
-                arg1=Binary.new(
-                    op="-",
-                    arg1=Binary.new("*", self.arg1.diff(), self.arg2),
-                    arg2=Binary.new("*", self.arg1, self.arg2.diff()),
+        # Case (power): f(x)^a
+        if isinstance(self.arg2, Const):
+            df = Mul.apply(
+                arg1=self.arg2,
+                arg2=Pow.apply(
+                    arg1=self.arg1, arg2=Const(self.arg2.value - 1)
                 ),
-                arg2=Binary.new("^", self.arg2, Const(2)),
             )
+            return df.chain(inner=self.arg1)
 
-        if self.op == "^":
-
-            # Case: a^f(x)
-            if isinstance(self.arg1, Const):
-                df = Binary.new(
-                    op="*",
-                    arg1=self,
-                    arg2=Unary(fn="ln", arg=self.arg1),
-                )
-                return df.chain(inner=self.arg2)
-
-            # Case: f(x)^a
-            if isinstance(self.arg2, Const):
-                df = Binary.new(
-                    op="*",
-                    arg1=self.arg2,
-                    arg2=Binary.new(
-                        op="^", arg1=self.arg1, arg2=Const(self.arg2.value - 1)
-                    ),
-                )
-                return df.chain(inner=self.arg1)
-
-            return self
-
-        raise ValueError("Unsupported operation {op}".format(op=self.op))
-
-    def __str__(self) -> str:
-        return "({op} {x} {y})".format(op=self.op, x=self.arg1, y=self.arg2)
+        # Default case (exponential): e^x
+        return self
 
 
 def parse(expression: str) -> Expr:
@@ -241,9 +337,9 @@ def parse(expression: str) -> Expr:
             expr1 = match.group("expr1")
             expr2 = match.group("expr2")
 
-        return Binary(op, arg1=parse(expr1), arg2=parse(expr2))
+        return Binary.new(op, arg1=parse(expr1), arg2=parse(expr2))
 
-    return Unary(fn=op, arg=parse(args))
+    return Unary.new(fn=op, arg=parse(args))
 
 
 def diff(expr: str) -> str:
